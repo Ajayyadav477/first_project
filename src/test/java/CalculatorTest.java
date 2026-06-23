d822af942cde9e53157fd3b804d9d2f7ebef6185
Ajay yadav <ajay.yadav@pay10.in>
2026-06-23 11:50:20 +0530

    initial commit

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,23 @@
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAdd() {
+        Calculator calc = new Calculator();
+        assertEquals(calc.add(2, 3), 5);
+    }
+
+    @Test
+    public void testSub() {
+        Calculator calc = new Calculator();
+        assertEquals(calc.sub(5, 2), 3);
+    }
+
+    @Test
+    public void testMul() {
+        Calculator calc = new Calculator();
+        assertEquals(calc.mul(2, 3), 6);
+    }
+}
